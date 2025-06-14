f25e456d3c6e8533ccea987c265cfd75dbc0d211
jessiecantrell <dcant322@gmail.com>
2025-06-14 10:33:57 -0500

    Completed 2-4

--- tests.py
+++ tests.py
@@ -1,17 +1,16 @@
-from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 def test():
-    result = get_file_content("calculator", "main.py")
-    print("Result for current main.py:")
-    print(result)
-    print("")
-
-    result = get_file_content("calculator", "pkg/calculator.py")
-    print("Result for 'pkg/calculator.py")
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    print("Result for lorem.txt:")
     print(result)
 
-    result = get_file_content("calculator", "/bin/cat")
-    print("Result for '/bin/cat")
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    print("Result for 'pkg/morelorem.txt")
+    print(result)
+
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    print("Result for '/tmp/text.txt")
     print(result)
 
 
